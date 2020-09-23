3a7404f15185f74a3bf108b5c0ea4f67222609a1
Kwangyoung Kim <blackdog0403@gmail.com>
2020-09-24 01:36:27 +0900

    modify image version

--- main.ts
+++ main.ts
@@ -14,7 +14,7 @@ export class MyChart extends Chart {
     super(scope, name);
 
     new DeboredApp(this, 'podinfo3', {
-      image: '245775375184.dkr.ecr.ap-northeast-1.amazonaws.com/cdk8s-demo-podinfo:d1439115',
+      image: '245775375184.dkr.ecr.ap-northeast-1.amazonaws.com/cdk8s-demo-podinfo:93f85566',
       containerPort: 9898,
       namespace: 'default',
       defaultReplicas: 2,
@@ -24,6 +24,7 @@ export class MyChart extends Chart {
         'flux.weave.works/automated' : 'true'
       }
     });
+    
     new HelmRelease(this, 'nginx', {
       metadata: {
         name: 'nginx',
